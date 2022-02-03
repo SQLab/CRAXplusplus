4a4bc2a28cfa7693c4352883b539234052f07bd8
Marco Wang <m.aesophor@gmail.com>
2022-02-04 01:03:31 +0800

    scripts/set-target.sh: create target and poc symlinks in examples/
    Signed-off-by: Marco Wang <m.aesophor@gmail.com>

--- scripts/set-target.sh
+++ scripts/set-target.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+# Copyright 2021-2022 Software Quality Laboratory, NYCU.
 
 if [ $# -lt 1 ]; then
   echo "usage: $0 <target>"
   exit 0
 fi
 
-ln -sf "../../examples/$1/$1" target
-ln -sf "../../examples/$1/poc" poc
+ln -sf "$HOME/s2e/source/CRAXplusplus/examples/$1/$1" target
+ln -sf "$HOME/s2e/source/CRAXplusplus/examples/$1/poc" poc

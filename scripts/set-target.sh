7926f0e031bf2ed79522199e1df9b5e7fde0a236
Marco Wang <m.aesophor@gmail.com>
2022-02-17 01:08:26 +0800

    move b64 to examples-staging
    Signed-off-by: Marco Wang <m.aesophor@gmail.com>

--- scripts/set-target.sh
+++ scripts/set-target.sh
@@ -1,10 +1,45 @@
 #!/usr/bin/env bash
 # Copyright 2021-2022 Software Quality Laboratory, NYCU.
 
+function usage() {
+    echo "usage: $0 [-s|--staging] <target>"
+    echo "examples:"
+    echo "$0 unexploitable"
+    echo "$0 --staging b64"
+}
+
+function create_symlinks() {
+    # $1: directory name (either examples or examples-staging)
+    # $2: target binary name (e.g., unexploitable)
+    ln -sfv "$HOME/s2e/source/CRAXplusplus/$1/$2/$2" "target"
+    ln -sfv "$HOME/s2e/source/CRAXplusplus/$1/$2/poc" "poc"
+}
+
+
 if [ $# -lt 1 ]; then
-  echo "usage: $0 <target>"
-  exit 0
+    usage
+    exit 0
 fi
 
-ln -sf "$HOME/s2e/source/CRAXplusplus/examples/$1/$1" target
-ln -sf "$HOME/s2e/source/CRAXplusplus/examples/$1/poc" poc
+# Parse command-line options
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -s|--staging)
+            create_symlinks "examples-staging" $2
+            exit 0
+            ;;
+        -*|--*)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+        *)
+            echo $1
+            create_symlinks "examples" $1
+            exit 0
+            ;;
+    esac
+done
